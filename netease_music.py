ad23c08dff89d31a164ca33740282d81dbdaa9d2
livexia <istillalive@live.cn>
2017-04-16 16:56:10 +0800

    rename getlist.py to netease_music.py

--- netease_music.py
+++ netease_music.py
@@ -32,21 +32,23 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0 Iceweasel/38.3.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
 }
-# play_url = 'http://music.163.com/playlist?id='
 
-play_url = 'http://music.163.com/playlist?id=' + sys.argv[1]
-list = getlist( play_url)
-for key in list:
-    print('歌曲id：',key)
-    print('     歌曲标题：'+list[key][0])
-    print('     歌手：'+list[key][1])
-    print('     歌手id：',list[key][2])
-    print('     专辑：'+list[key][3])
-    print('     专辑id：',list[key][4])
-    print('     Url：')
-    print('          歌曲链接：'+list[key][5][0])
-    print('          歌手链接：'+list[key][5][1])
-    print('          专辑链接：'+list[key][5][2])
-    print('          专辑图片：'+list[key][5][3])
+
+# play_url = 'http://music.163.com/playlist?id='
+if sys.argv[1] == 'playlist':
+    play_url = 'http://music.163.com/playlist?id=' + sys.argv[2]
+    list = getlist( play_url)
+    for key in list:
+        print('歌曲id：',key)
+        print('     歌曲标题：'+list[key][0])
+        print('     歌手：'+list[key][1])
+        print('     歌手id：',list[key][2])
+        print('     专辑：'+list[key][3])
+        print('     专辑id：',list[key][4])
+        print('     Url：')
+        print('          歌曲链接：'+list[key][5][0])
+        print('          歌手链接：'+list[key][5][1])
+        print('          专辑链接：'+list[key][5][2])
+        print('          专辑图片：'+list[key][5][3])
 
 
